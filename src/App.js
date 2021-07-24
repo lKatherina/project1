b5b5778917fd9313bd2fd75e9de47d119e955c13
Grigory Koksharov <grigory.koksharov@revolut.com>
2021-07-24 14:16:44 +0300

    Auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { render } from "react-dom";
+import firebase from 'firebase/app'
+import "firebase/auth";
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd
+} from "@react-firebase/auth";
 
-function App() {
+const config = {
+  apiKey: "AIzaSyB2dYx7HNSKYcP0hDl_jZQ9Go5ILf82OlI",
+  authDomain: "graph-test-3a70f.firebaseapp.com",
+  projectId: "graph-test-3a70f",
+  storageBucket: "graph-test-3a70f.appspot.com",
+  messagingSenderId: "494812105664",
+  appId: "1:494812105664:web:de696c32d0647dd17f5625",
+  measurementId: "G-D9C1M3M7XY"
+};
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <FirebaseAuthProvider {...config} firebase={firebase}>
+      <div>
+        <button
+          onClick={() => {
+            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithPopup(googleAuthProvider);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Sign In with Google
+        </button>
+        <button
+          data-testid="signin-anon"
+          onClick={() => {
+            firebase.auth().signInAnonymously();
+          }}
+        >
+          Sign In Anonymously
+        </button>
+        <button
+          onClick={() => {
+            firebase.auth().signOut();
+          }}
+        >
+          Sign Out
+        </button>
+        <FirebaseAuthConsumer>
+          {({ isSignedIn, user, providerId }) => {
+            return (
+              <pre style={{ height: 300, overflow: "auto" }}>
+                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+              </pre>
+            );
+          }}
+        </FirebaseAuthConsumer>
+        <div>
+          <IfFirebaseAuthed>
+            {() => {
+              return <div>You are authenticated</div>;
+            }}
+          </IfFirebaseAuthed>
+          <IfFirebaseAuthedAnd
+            filter={({ providerId }) => providerId !== "anonymous"}
+          >
+            {({ providerId }) => {
+              return <div>You are authenticated with {providerId}</div>;
+            }}
+          </IfFirebaseAuthedAnd>
+        </div>
+      </div>
+    </FirebaseAuthProvider>
   );
-}
+};
+render(<App />, document.getElementById("root"));
 
 export default App;
